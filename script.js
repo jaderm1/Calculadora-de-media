9743d019f930f64f8838dcebdbc6587b6dfbfd5e
Jader <jadermaciel1@gmail.com>
2023-06-10 22:55:06 -0300

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let nota1 = prompt("insira o valor da primeira nota ")
+
+let nota2 = prompt("insira o valor da segunda nota")
+
+let nota3 = prompt("insira o valor da terceira nota")
+
+let nota4 = prompt("insira o valor da quarta nota")
+
+
+let resultado = (parseFloat(nota1) + parseFloat(nota2)+parseFloat(nota3) + parseFloat(nota4))/4
+
+
+alert("Sua media é "+resultado);
